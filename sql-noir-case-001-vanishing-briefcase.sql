e15561abef6f4caf3a835a2abb507381b7261b12
Dharshclin24 <ShavonFdclinton@gmail.com>
2025-06-15 18:27:17 -0700

    Update sql-noir-case-001-vanishing-briefcase.sql

--- sql-noir-case-001-vanishing-briefcase.sql
+++ sql-noir-case-001-vanishing-briefcase.sql
@@ -1,3 +1,18 @@
+--Set in the gritty 1980s, a valuable briefcase has disappeared from the Blue Note Lounge. A witness reported that a man in a trench coat was seen fleeing the scene. Investigate the crime scene, review the list of suspects, and examine interview transcripts to reveal the culprit.
+
+--SELECT * FROM crime_scene  WHERE location = 'Blue Note Lounge';
+
+--SELECT * FROM suspects WHERE scar = 'left cheek';
+--3	Frankie Lombardi	trench coat	left cheek
+--183	Vincent Malone	trench coat	left cheek
+
+SELECT * FROM  interviews;
+--183	I wasn’t going to steal it, but I did.
+ he did it Vincent Malone
+
+
+
+
 -- Record your SQL detective process here!  Write down: 
   -- 1. The SQL queries you ran
   -- 2. Any notes or insights as SQL comments
